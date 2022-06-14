ed238db58e4f7c123a083223c12fd909db021196
danaelhe <42972711+danaelhe@users.noreply.github.com>
2022-06-14 17:01:10 -0400

    Create Function to Surface IP Address for Droplets

--- do-client-python/digitalocean/operations/_patch.py
+++ do-client-python/digitalocean/operations/_patch.py
@@ -6,13 +6,20 @@
 
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
 """
-from typing import TYPE_CHECKING
+from typing import List
+from ._operations import DropletsOperations as Droplets
 
-if TYPE_CHECKING:
-    # pylint: disable=unused-import,ungrouped-imports
-    from typing import List
+class DropletsOperations(Droplets):
 
-__all__ = []  # type: List[str]  # Add all objects you want publicly available to users at this package level
+    @classmethod
+    def get_public_IPv4(self, droplet):
+        ip_address = ''
+        for net in droplet['networks']['v4']:
+            if net['type'] == 'public':
+                ip_address = net['ip_address']
+        return ip_address
+
+__all__ = ["DropletsOperations"]
 
 def patch_sdk():
     """Do not remove from this file.
