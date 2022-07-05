1ef5b850991f43429984f52f248d718d30001f40
Cesar Garza <scotch.neat@live.com>
2022-07-05 18:56:59 -0500

    Add mocked and integration tests for kubernetes

---
+++ tests/integration/test_kubernetes.py
@@ -0,0 +1,47 @@
+from time import sleep
+import uuid
+
+import defaults
+import shared
+from digitalocean import DigitalOceanClient
+
+
+def test_create_cluster(integration_client: DigitalOceanClient):
+    node_pool = "worker-pool"
+
+    create_req = {
+        "name": f"{defaults.PREFIX}-cluster-{uuid.uuid4()}",
+        "region": defaults.REGION,
+        "version": defaults.K8S_VERSION,
+        "node_pools": [{"size": defaults.K8S_NODE_SIZE, "count": 2, "name": node_pool}],
+    }
+
+    with shared.with_test_kubernetes_cluster(integration_client, **create_req) as kc:
+        cluster_id = kc["kubernetes_cluster"]["id"] or ""
+        assert cluster_id != ""
+        node_pool_id = kc["kubernetes_cluster"]["node_pools"][0]["id"] or ""
+        assert node_pool_id != ""
+
+        shared.wait_for_kubernetes_cluster_create(integration_client, cluster_id)
+
+        # get the cluster info and verify the state
+        get_resp = integration_client.kubernetes.get_cluster(cluster_id)
+        assert get_resp["kubernetes_cluster"]["status"]["state"] == "running"
+
+        # update the number of nodes in the node pool
+        update_node_pool_resp = integration_client.kubernetes.update_node_pool(
+            cluster_id, node_pool_id, {"name": node_pool, "count": 4}
+        )
+        assert update_node_pool_resp["node_pool"]["count"] == 4
+
+        # get the cluster info again to verify the number of nodes changed
+        get_node_pool_resp = integration_client.kubernetes.get_node_pool(
+            cluster_id, node_pool
+        )
+        assert get_node_pool_resp["node_pool"]["count"] == 4
+
+        # get the kubeconfig
+        get_kubeconfig_resp = integration_client.kubernetes.get_kubeconfig(
+            cluster_id, node_pool_id
+        )
+        assert get_kubeconfig_resp[:11] == "apiVersion:"
