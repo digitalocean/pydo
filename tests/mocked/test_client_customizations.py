66d5f7ee3e7ef41e0300aa0fc9b22e90fdc7d1a0
Cesar Garza <scotch.neat@live.com>
2022-07-11 12:57:58 -0500

    Client customization examples (#20)
    * Handle package version in makefile
    
    Also cleaned up the pytest args to be more flexible.
    
    * Added a custom HttpLoggingPolicy
    
    Used a custom_policies module so they can be shared across the regular and aio client
    
    * Fixed incorrect condition
    
    * Ignore log files
    
    * Use shared CustomHttpLoggingPolicy
    
    * Add examples for customizing client settings
    
    * Include client customization details in README

---
+++ tests/mocked/test_client_customizations.py
@@ -0,0 +1,74 @@
+"""Client customization tests
+
+These tests aren't essential but serve as good examples for using the client with
+custom configuration.
+"""
+import logging
+import re
+
+import pytest
+import responses
+from digitalocean import DigitalOceanClient
+
+
+def test_custom_headers():
+    custom_headers = {"x-request-id": "fakeid"}
+    client = DigitalOceanClient("", headers=custom_headers)
+
+    # pylint: disable=protected-access
+    assert client._client._config.headers_policy.headers == custom_headers
+
+
+def test_custom_timeout():
+    timeout = 300
+    client = DigitalOceanClient("", timeout=timeout)
+
+    # pylint: disable=protected-access
+    assert client._client._config.retry_policy.timeout == timeout
+
+
+def test_custom_endpoint():
+    endpoint = "https://fake.local"
+    client = DigitalOceanClient("", endpoint=endpoint)
+
+    # pylint: disable=protected-access
+    assert client._client._base_url == endpoint
+
+
+def test_custom_logger():
+    name = "mockedtests"
+    logger = logging.getLogger(name)
+    client = DigitalOceanClient("", logger=logger)
+
+    # pylint: disable=protected-access
+    assert client._client._config.http_logging_policy.logger.name == name
+
+
+@responses.activate
+def test_custom_user_agent():
+    user_agent = "test"
+    fake_endpoint = "https://fake.local"
+    client = DigitalOceanClient(
+        "",
+        endpoint=fake_endpoint,
+        user_agent=user_agent,
+        user_agent_overwrite=True,
+    )
+
+    full_user_agent_pattern = (
+        f"^{user_agent} azsdk-python-digitaloceanclient\/.+Python\/.+\(.+\)$"
+    )
+    # pylint: disable=protected-access
+    got_user_agent = client._client._config.user_agent_policy.user_agent
+    match = re.match(full_user_agent_pattern, got_user_agent)
+    assert match is not None
+
+    fake_url = f"{fake_endpoint}/v2/account"
+    responses.add(
+        responses.GET,
+        fake_url,
+        match=[responses.matchers.header_matcher({"User-Agent": user_agent})],
+    )
+
+    client.account.get(user_agent=user_agent)
+    assert responses.assert_call_count(fake_url, count=1)
