2b0db670b55ad956c325a5dac8b78d81f88974cd
Samuel Guyah <septimusguy@gmail.com>
2022-07-14 15:54:48 -0500

    fixed lines

--- tests/mocked/test_domains.py
+++ tests/mocked/test_domains.py
@@ -6,7 +6,7 @@ from digitalocean import DigitalOceanClient
 
 
 @responses.activate
-def test_create_record(mock_client: DigitalOceanClient, mock_client_url):
+def test_create(mock_client: DigitalOceanClient, mock_client_url):
     """Tests Record Creation"""
     expected = {"domain": {"name": "clienttest.com", "ttl": 1800, "zone_file": ""}}
 
@@ -28,7 +28,12 @@ def test_get(mock_client: DigitalOceanClient, mock_client_url):
     expected = {
         "name": "clienttest.com",
         "ttl": 1800,
-        "zone_file": "$ORIGIN clienttest.com.\n$TTL 1800\nclienttest.com. IN SOA ns1.digitalocean.com. hostmaster.clienttest.com. 1657812556 10800 3600 604800 1800\nclienttest.com. 1800 IN NS ns1.digitalocean.com.\nclienttest.com. 1800 IN NS ns2.digitalocean.com.\nclienttest.com. 1800 IN NS ns3.digitalocean.com.\n",
+        "zone_file": """$ORIGIN clienttest.com.\n$TTL 1800\nclienttest.com. \
+            IN SOA ns1.digitalocean.com. \
+            hostmaster.clienttest.com. 1657812556 10800 3600 604800 1800\nclienttest.com. \
+            1800 IN NS ns1.digitalocean.com. \
+            \nclienttest.com. 1800 IN NS ns2.digitalocean.com.\nclienttest.com. \
+            1800 IN NS ns3.digitalocean.com.\n""",
     }
 
     responses.add(
@@ -50,7 +55,12 @@ def test_list(mock_client: DigitalOceanClient, mock_client_url):
         "domains": {
             "name": "clienttest.com",
             "ttl": 1800,
-            "zone_file": "$ORIGIN clienttest.com.\n$TTL 1800\nclienttest.com. IN SOA ns1.digitalocean.com. hostmaster.clienttest.com. 1657812556 10800 3600 604800 1800\nclienttest.com. 1800 IN NS ns1.digitalocean.com.\nclienttest.com. 1800 IN NS ns2.digitalocean.com.\nclienttest.com. 1800 IN NS ns3.digitalocean.com.\n",
+            "zone_file": """$ORIGIN clienttest.com.\n$TTL 1800\nclienttest.com. \
+                IN SOA ns1.digitalocean.com. \
+                hostmaster.clienttest.com. 1657812556 10800 3600 604800 1800\nclienttest.com. \
+                1800 IN NS ns1.digitalocean.com. \
+                \nclienttest.com. 1800 IN NS ns2.digitalocean.com.\nclienttest.com. \
+                1800 IN NS ns3.digitalocean.com.\n""",
         },
         "links": {},
         "meta": {"total": 1},
@@ -79,13 +89,13 @@ def test_delete(mock_client: DigitalOceanClient, mock_client_url):
     )
     del_resp = mock_client.domains.delete("testtclient.com")
 
-    assert del_resp == None
+    assert del_resp is None
 
 
 @responses.activate
 def test_create_record(mock_client: DigitalOceanClient, mock_client_url):
     """Test Record Create"""
-    excepted = {
+    expected = {
         "domain_record": {
             "id": 324119029,
             "type": "A",
@@ -103,7 +113,7 @@ def test_create_record(mock_client: DigitalOceanClient, mock_client_url):
     responses.add(
         responses.POST,
         f"{mock_client_url}/v2/domains/ec.com/records",
-        json=excepted,
+        json=expected,
         status=201,
     )
 
@@ -122,10 +132,158 @@ def test_create_record(mock_client: DigitalOceanClient, mock_client_url):
         },
     )
 
-    assert create_resp == excepted
+    assert create_resp == expected
+
+
+@responses.activate
+def test_list_records(mock_client: DigitalOceanClient, mock_client_url):
+    """Test Record Domain List"""
+    expected = {
+        "domain_records": [
+            {
+                "id": 324119029,
+                "type": "A",
+                "name": "ec.com",
+                "data": "162.10.66.0",
+                "priority": None,
+                "port": None,
+                "ttl": 1800,
+                "weight": None,
+                "flags": None,
+                "tag": None,
+            },
+        ],
+        "links": {},
+        "meta": {"total": 1},
+    }
+
+    responses.add(
+        responses.GET,
+        f"{mock_client_url}/v2/domains/ec.com/records",
+        json=expected,
+        status=200,
+    )
+
+    list_resp = mock_client.domains.list_records("ec.com")
+
+    assert list_resp == expected
 
 
 @responses.activate
 def test_get_record(mock_client: DigitalOceanClient, mock_client_url):
     """Test Record Domain Get"""
-    pass
+
+    expected = {
+        "domain_record": {
+            "id": 324119029,
+            "type": "A",
+            "name": "ec.com",
+            "data": "162.10.66.0",
+            "priority": None,
+            "port": None,
+            "ttl": 1800,
+            "weight": None,
+            "flags": None,
+            "tag": None,
+        },
+    }
+
+    responses.add(
+        responses.GET,
+        f"{mock_client_url}/v2/domains/ec.com/records/324119029",
+        json=expected,
+        status=200,
+    )
+
+    get_resp = mock_client.domains.get_record("ec.com", 324119029)
+
+    assert get_resp == expected
+
+
+@responses.activate
+def test_update_record(mock_client: DigitalOceanClient, mock_client_url):
+    """Test Record Domain Update"""
+    expected = {
+        "domain_record": {
+            "id": 324119029,
+            "type": "A",
+            "name": "ec.com",
+            "data": "162.10.66.0",
+            "priority": None,
+            "port": None,
+            "ttl": 1800,
+            "weight": None,
+            "flags": None,
+            "tag": None,
+        },
+    }
+
+    responses.add(
+        responses.PUT,
+        f"{mock_client_url}/v2/domains/ec.com/records/324119029",
+        json=expected,
+        status=200,
+    )
+
+    update_resp = mock_client.domains.update_record(
+        "ec.com",
+        324119029,
+        {
+            "name": "ec.com",
+            "type": "A",
+        },
+    )
+
+    assert update_resp == expected
+
+
+@responses.activate
+def test_patch_record(mock_client: DigitalOceanClient, mock_client_url):
+    """Test Record Domain Update"""
+    expected = {
+        "domain_record": {
+            "id": 324119029,
+            "type": "A",
+            "name": "ec.com",
+            "data": "162.10.66.0",
+            "priority": None,
+            "port": None,
+            "ttl": 1800,
+            "weight": None,
+            "flags": None,
+            "tag": None,
+        },
+    }
+
+    responses.add(
+        responses.PATCH,
+        f"{mock_client_url}/v2/domains/ec.com/records/324119029",
+        json=expected,
+        status=200,
+    )
+
+    patch_resp = mock_client.domains.patch_record(
+        "ec.com",
+        324119029,
+        {
+            "name": "ec.com",
+            "type": "A",
+        },
+    )
+
+    assert patch_resp == expected
+
+
+@responses.activate
+def test_delete_record(mock_client: DigitalOceanClient, mock_client_url):
+    """Test Domain Record Delete"""
+
+    responses.add(
+        responses.DELETE,
+        f"{mock_client_url}/v2/domains/ec.com/records/324119029",
+        status=204,
+    )
+
+    del_resp = mock_client.domains.delete_record("ec.com", 324119029)
+
+    assert del_resp is None
