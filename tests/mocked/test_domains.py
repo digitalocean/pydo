bcd416e10f1424beb8398fa28be506d92fe51681
Samuel Guyah <septimusguy@gmail.com>
2022-07-14 10:56:40 -0500

    first test

--- tests/mocked/test_domains.py
+++ tests/mocked/test_domains.py
@@ -6,5 +6,16 @@ from digitalocean import DigitalOceanClient
 
 
 @responses.activate
-def test_create_record(mock_client: DigitalOceanClient):
-    pass
+def test_create_record(mock_client: DigitalOceanClient, mock_client_url):
+    expected = {"domain": {"name": "clienttest.com", "ttl": 1800, "zone_file": ""}}
+
+    responses.add(
+        responses.POST,
+        f"{mock_client_url}/V2/domains",
+        json=expected,
+        status=202,
+    )
+
+    create_resp = mock_client.domains.create({"name": "clienttest.com"})
+
+    assert create_resp == expected
