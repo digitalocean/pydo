cd1c9e14a18c64cdb14ac276742f88110d69835d
Samuel Guyah <septimusguy@gmail.com>
2022-07-19 16:43:25 -0500

    actions mocked

---
+++ tests/mocked/test_actions.py
@@ -0,0 +1,57 @@
+"""Mock tests for the actions API resource"""
+import json
+import responses
+
+from digitalocean import DigitalOceanClient
+
+
+@responses.activate
+def test_list(mock_client: DigitalOceanClient, mock_client_url):
+    """Mocks the actions list operation."""
+    expected = {
+        "actions": [
+            {
+                "id": 36804636,
+                "status": "completed",
+                "type": "create",
+                "started_at": "2020-11-14T16:29:21Z",
+                "completed_at": "2020-11-14T16:30:06Z",
+                "resource_id": 3164444,
+                "resource_type": "droplet",
+                "region": {},
+                "region_slug": "string",
+            },
+        ]
+    }
+
+    responses.add(responses.GET, f"{mock_client_url}/v2/actions", json=expected)
+    list_resp = mock_client.actions.list()
+
+    assert list_resp == expected
+
+
+@responses.activate
+def test_get(mock_client: DigitalOceanClient, mock_client_url):
+    """Mocks the actions get operation."""
+
+    action_id = 36804636
+    expected = {
+        "actions": {
+            "id": action_id,
+            "status": "completed",
+            "type": "create",
+            "started_at": "2020-11-14T16:29:21Z",
+            "completed_at": "2020-11-14T16:30:06Z",
+            "resource_id": 3164444,
+            "resource_type": "droplet",
+            "region": {},
+            "region_slug": "string",
+        },
+    }
+
+    responses.add(
+        responses.GET, f"{mock_client_url}/v2/actions/{action_id}", json=expected
+    )
+    get_resp = mock_client.actions.get(action_id)
+
+    assert get_resp == expected
