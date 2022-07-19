1379daafe43ce872c815498010b2ac320dbf0692
Samuel Guyah <septimusguy@gmail.com>
2022-07-19 17:52:28 -0500

    len

--- tests/integration/test_regions.py
+++ tests/integration/test_regions.py
@@ -10,4 +10,4 @@ def test_regions(integration_client: DigitalOceanClient):
 
     list_resp = integration_client.regions.list()
 
-    assert list_resp >= 13
+    assert len(list_resp["regions"]) >= 13
