440b67fefe06d9bcd02ce858bf9175e1b9296ba0
Andrew Starr-Bochicchio <andrewsomething@users.noreply.github.com>
2022-07-25 13:21:16 -0400

    firewalls: Add mocked and integration tests. (#48)
    * firewalls: Add mocked tests.
    
    * firewalls: Add integration tests.
    
    * Fix linter errors.

---
+++ tests/integration/test_firewalls.py
@@ -0,0 +1,93 @@
+# pylint: disable=duplicate-code
+
+""" test_firewalls.py
+    Integration tests for firewalls.
+"""
+
+import uuid
+
+from tests.integration import defaults, shared
+from digitalocean import Client
+
+
+def test_firewalls(integration_client: Client):
+    """Tests creating, updating, and deleting a firewall"""
+    tag_name = f"{defaults.PREFIX}-{uuid.uuid4()}"
+    firewall_name = f"{defaults.PREFIX}-{uuid.uuid4()}"
+    tag_body = {"name": tag_name}
+
+    with shared.with_test_tag(integration_client, **tag_body):
+        try:
+            create_req = {
+                "name": firewall_name,
+                "outbound_rules": [
+                    {
+                        "protocol": "tcp",
+                        "ports": "80",
+                        "destinations": {"addresses": ["0.0.0.0/0", "::/0"]},
+                    }
+                ],
+                "tags": [tag_name],
+            }
+
+            # Create firewall
+            firewall = integration_client.firewalls.create(body=create_req)
+            firewall_id = firewall["firewall"]["id"]
+            assert firewall_id is not None
+            assert firewall["firewall"]["name"] == firewall_name
+            assert firewall["firewall"]["tags"] == [tag_name]
+            assert firewall["firewall"]["outbound_rules"][0]["protocol"] == "tcp"
+            assert firewall["firewall"]["outbound_rules"][0]["ports"] == "80"
+            assert firewall["firewall"]["outbound_rules"][0]["destinations"] == {
+                "addresses": ["0.0.0.0/0", "::/0"]
+            }
+
+            # GET firewall
+            got = integration_client.firewalls.get(firewall_id=firewall_id)
+            assert firewall_id == got["firewall"]["id"]
+            assert got["firewall"]["name"] == firewall_name
+            assert got["firewall"]["tags"] == [tag_name]
+            assert got["firewall"]["outbound_rules"][0]["protocol"] == "tcp"
+            assert got["firewall"]["outbound_rules"][0]["ports"] == "80"
+            assert got["firewall"]["outbound_rules"][0]["destinations"] == {
+                "addresses": ["0.0.0.0/0", "::/0"]
+            }
+
+            # Add rule
+            rule = {
+                "inbound_rules": [
+                    {
+                        "protocol": "tcp",
+                        "ports": "2222",
+                        "sources": {"addresses": ["0.0.0.0/0", "::/0"]},
+                    }
+                ]
+            }
+            integration_client.firewalls.add_rules(firewall_id=firewall_id, body=rule)
+            updated = integration_client.firewalls.get(firewall_id=firewall_id)
+            assert updated["firewall"]["inbound_rules"][0]["protocol"] == "tcp"
+            assert updated["firewall"]["inbound_rules"][0]["ports"] == "2222"
+            assert updated["firewall"]["inbound_rules"][0]["sources"] == {
+                "addresses": ["0.0.0.0/0", "::/0"]
+            }
+
+            # Remove rule
+            remove = {
+                "outbound_rules": [
+                    {
+                        "protocol": "tcp",
+                        "ports": "80",
+                        "destinations": {"addresses": ["0.0.0.0/0", "::/0"]},
+                    }
+                ]
+            }
+            integration_client.firewalls.delete_rules(
+                firewall_id=firewall_id, body=remove
+            )
+            removed = integration_client.firewalls.get(firewall_id=firewall_id)
+            assert len(removed["firewall"]["outbound_rules"]) == 0
+
+        finally:
+            # Delete firewall
+            if firewall_id is not None:
+                integration_client.firewalls.delete(firewall_id=firewall_id)
---
+++ tests/mocked/test_firewalls.py
@@ -0,0 +1,383 @@
+"""Mock tests for the firewalls API resource."""
+import responses
+
+from digitalocean import Client
+
+
+@responses.activate
+def test_list_firewalls(mock_client: Client, mock_client_url):
+    """Mocks the firewalls list operation."""
+    expected = {
+        "firewalls": [
+            {
+                "id": "e8721de7-ebd9-46ff-8b8d-f6cd14b2769b",
+                "name": "public-access",
+                "status": "succeeded",
+                "inbound_rules": [],
+                "outbound_rules": [
+                    {
+                        "protocol": "icmp",
+                        "ports": "0",
+                        "destinations": {"addresses": ["0.0.0.0/0", "::/0"]},
+                    },
+                    {
+                        "protocol": "tcp",
+                        "ports": "0",
+                        "destinations": {"addresses": ["0.0.0.0/0", "::/0"]},
+                    },
+                    {
+                        "protocol": "udp",
+                        "ports": "0",
+                        "destinations": {"addresses": ["0.0.0.0/0", "::/0"]},
+                    },
+                ],
+                "created_at": "2021-10-11T19:04:13Z",
+                "droplet_ids": [],
+                "tags": ["public-access"],
+                "pending_changes": [],
+            },
+            {
+                "id": "fb6045f1-cf1d-4ca3-bfac-18832663025b",
+                "name": "firewall",
+                "status": "succeeded",
+                "inbound_rules": [
+                    {
+                        "protocol": "tcp",
+                        "ports": "80",
+                        "sources": {
+                            "load_balancer_uids": [
+                                "4de7ac8b-495b-4884-9a69-1050c6793cd6"
+                            ]
+                        },
+                    },
+                    {
+                        "protocol": "tcp",
+                        "ports": "22",
+                        "sources": {"tags": ["gateway"], "addresses": ["18.0.0.0/8"]},
+                    },
+                ],
+                "created_at": "2017-05-23T21:23:59Z",
+                "droplet_ids": [123456],
+                "tags": [],
+                "pending_changes": [],
+            },
+        ],
+        "links": {},
+        "meta": {"total": 2},
+    }
+
+    responses.add(responses.GET, f"{mock_client_url}/v2/firewalls", json=expected)
+    firewalls = mock_client.firewalls.list()
+
+    assert firewalls == expected
+
+
+@responses.activate
+def test_get_firewalls(mock_client: Client, mock_client_url):
+    """Mocks the firewalls get operation."""
+    expected = {
+        "firewall": {
+            "id": "fb6045f1-cf1d-4ca3-bfac-18832663025b",
+            "name": "firewall",
+            "status": "succeeded",
+            "inbound_rules": [
+                {
+                    "protocol": "tcp",
+                    "ports": "80",
+                    "sources": {
+                        "load_balancer_uids": ["4de7ac8b-495b-4884-9a69-1050c6793cd6"]
+                    },
+                },
+                {
+                    "protocol": "tcp",
+                    "ports": "22",
+                    "sources": {"tags": ["gateway"], "addresses": ["18.0.0.0/8"]},
+                },
+            ],
+            "created_at": "2017-05-23T21:23:59Z",
+            "droplet_ids": [123456],
+            "tags": [],
+            "pending_changes": [],
+        }
+    }
+
+    responses.add(
+        responses.GET,
+        f"{mock_client_url}/v2/firewalls/fb6045f1-cf1d-4ca3-bfac-18832663025b",
+        json=expected,
+    )
+    firewalls = mock_client.firewalls.get(
+        firewall_id="fb6045f1-cf1d-4ca3-bfac-18832663025b"
+    )
+
+    assert firewalls == expected
+
+
+@responses.activate
+def test_create_firewalls(mock_client: Client, mock_client_url):
+    """Mocks the firewalls create operation."""
+    expected = {
+        "firewall": {
+            "id": "bb4b2611-3d72-467b-8602-280330ecd65c",
+            "name": "firewall",
+            "status": "waiting",
+            "inbound_rules": [
+                {
+                    "protocol": "tcp",
+                    "ports": "80",
+                    "sources": {
+                        "load_balancer_uids": ["4de7ac8b-495b-4884-9a69-1050c6793cd6"]
+                    },
+                },
+                {
+                    "protocol": "tcp",
+                    "ports": "22",
+                    "sources": {"tags": ["gateway"], "addresses": ["18.0.0.0/8"]},
+                },
+            ],
+            "outbound_rules": [
+                {
+                    "protocol": "tcp",
+                    "ports": "80",
+                    "destinations": {"addresses": ["0.0.0.0/0", "::/0"]},
+                }
+            ],
+            "created_at": "2017-05-23T21:24:00Z",
+            "droplet_ids": [8043964],
+            "tags": [],
+            "pending_changes": [
+                {"droplet_id": 8043964, "removing": False, "status": "waiting"}
+            ],
+        }
+    }
+
+    responses.add(
+        responses.POST,
+        f"{mock_client_url}/v2/firewalls",
+        json=expected,
+        status=202,
+    )
+
+    create_req = {
+        "name": "firewall",
+        "inbound_rules": [
+            {
+                "protocol": "tcp",
+                "ports": "80",
+                "sources": {
+                    "load_balancer_uids": ["4de7ac8b-495b-4884-9a69-1050c6793cd6"]
+                },
+            },
+            {
+                "protocol": "tcp",
+                "ports": "22",
+                "sources": {"tags": ["gateway"], "addresses": ["18.0.0.0/8"]},
+            },
+        ],
+        "outbound_rules": [
+            {
+                "protocol": "tcp",
+                "ports": "80",
+                "destinations": {"addresses": ["0.0.0.0/0", "::/0"]},
+            }
+        ],
+        "droplet_ids": [8043964],
+    }
+    firewall = mock_client.firewalls.create(body=create_req)
+
+    assert firewall == expected
+
+
+@responses.activate
+def test_update_firewalls(mock_client: Client, mock_client_url):
+    """Mocks the firewalls update operation."""
+    expected = {
+        "firewall": {
+            "id": "bb4b2611-3d72-467b-8602-280330ecd65c",
+            "name": "frontend-firewall",
+            "inbound_rules": [
+                {
+                    "protocol": "tcp",
+                    "ports": "80",
+                    "sources": {
+                        "load_balancer_uids": ["4de7ac8b-495b-4884-9a69-1050c6793cd6"]
+                    },
+                },
+                {
+                    "protocol": "tcp",
+                    "ports": "22",
+                    "sources": {"tags": ["gateway"], "addresses": ["18.0.0.0/8"]},
+                },
+            ],
+            "outbound_rules": [
+                {
+                    "protocol": "tcp",
+                    "ports": "80",
+                    "destinations": {"addresses": ["0.0.0.0/0", "::/0"]},
+                }
+            ],
+            "created_at": "2020-05-23T21:24:00Z",
+            "droplet_ids": [8043964],
+            "tags": ["frontend"],
+            "status": "waiting",
+            "pending_changes": [
+                {"droplet_id": 8043964, "removing": False, "status": "waiting"}
+            ],
+        }
+    }
+    responses.add(
+        responses.PUT,
+        f"{mock_client_url}/v2/firewalls/bb4b2611-3d72-467b-8602-280330ecd65c",
+        json=expected,
+        status=200,
+    )
+
+    update = {
+        "name": "frontend-firewall",
+        "inbound_rules": [
+            {
+                "protocol": "tcp",
+                "ports": "8080",
+                "sources": {
+                    "load_balancer_uids": ["4de7ac8b-495b-4884-9a69-1050c6793cd6"]
+                },
+            },
+            {
+                "protocol": "tcp",
+                "ports": "22",
+                "sources": {"tags": ["gateway"], "addresses": ["18.0.0.0/8"]},
+            },
+        ],
+        "outbound_rules": [
+            {
+                "protocol": "tcp",
+                "ports": "8080",
+                "destinations": {"addresses": ["0.0.0.0/0", "::/0"]},
+            }
+        ],
+        "droplet_ids": [8043964],
+        "tags": ["frontend"],
+    }
+    firewall = mock_client.firewalls.update(
+        body=update, firewall_id="bb4b2611-3d72-467b-8602-280330ecd65c"
+    )
+
+    assert firewall == expected
+
+
+@responses.activate
+def test_delete_firewalls(mock_client: Client, mock_client_url):
+    """Mocks the firewalls delete operation."""
+    responses.add(
+        responses.DELETE,
+        f"{mock_client_url}/v2/firewalls/aaa-bbb-111-ccc-222",
+        status=204,
+    )
+
+    mock_client.firewalls.delete(firewall_id="aaa-bbb-111-ccc-222")
+
+
+@responses.activate
+def test_firewalls_assign_droplets(mock_client: Client, mock_client_url):
+    """Mocks the firewalls add Droplets operation."""
+    responses.add(
+        responses.POST,
+        f"{mock_client_url}/v2/firewalls/aaa-bbb-111-ccc-222/droplets",
+        status=204,
+    )
+
+    mock_client.firewalls.assign_droplets(
+        firewall_id="aaa-bbb-111-ccc-222", body={"droplet_ids": [1234, 5678]}
+    )
+
+
+@responses.activate
+def test_firewalls_delete_droplets(mock_client: Client, mock_client_url):
+    """Mocks the firewalls remove Droplets operation."""
+    responses.add(
+        responses.DELETE,
+        f"{mock_client_url}/v2/firewalls/aaa-bbb-111-ccc-222/droplets",
+        status=204,
+    )
+
+    mock_client.firewalls.delete_droplets(
+        firewall_id="aaa-bbb-111-ccc-222", body={"droplet_ids": [1234, 5678]}
+    )
+
+
+@responses.activate
+def test_firewalls_add_tags(mock_client: Client, mock_client_url):
+    """Mocks the firewalls add tags operation."""
+    responses.add(
+        responses.POST,
+        f"{mock_client_url}/v2/firewalls/aaa-bbb-111-ccc-222/tags",
+        status=204,
+    )
+
+    mock_client.firewalls.add_tags(
+        firewall_id="aaa-bbb-111-ccc-222", body={"tags": ["frontend"]}
+    )
+
+
+@responses.activate
+def test_firewalls_delete_tags(mock_client: Client, mock_client_url):
+    """Mocks the firewalls delete tags operation."""
+    responses.add(
+        responses.DELETE,
+        f"{mock_client_url}/v2/firewalls/aaa-bbb-111-ccc-222/tags",
+        status=204,
+    )
+
+    mock_client.firewalls.delete_tags(
+        firewall_id="aaa-bbb-111-ccc-222", body={"tags": ["frontend"]}
+    )
+
+
+@responses.activate
+def test_firewalls_add_rules(mock_client: Client, mock_client_url):
+    """Mocks the firewalls add rules operation."""
+    responses.add(
+        responses.POST,
+        f"{mock_client_url}/v2/firewalls/aaa-bbb-111-ccc-222/rules",
+        status=204,
+    )
+
+    rules = {
+        "inbound_rules": [
+            {"protocol": "tcp", "ports": "3306", "sources": {"droplet_ids": [49696269]}}
+        ],
+        "outbound_rules": [
+            {
+                "protocol": "tcp",
+                "ports": "3306",
+                "destinations": {"droplet_ids": [49696269]},
+            }
+        ],
+    }
+
+    mock_client.firewalls.add_rules(firewall_id="aaa-bbb-111-ccc-222", body=rules)
+
+
+@responses.activate
+def test_firewalls_delete_rules(mock_client: Client, mock_client_url):
+    """Mocks the firewalls delete rules operation."""
+    responses.add(
+        responses.DELETE,
+        f"{mock_client_url}/v2/firewalls/aaa-bbb-111-ccc-222/rules",
+        status=204,
+    )
+
+    rules = {
+        "inbound_rules": [
+            {"protocol": "tcp", "ports": "3306", "sources": {"droplet_ids": [49696269]}}
+        ],
+        "outbound_rules": [
+            {
+                "protocol": "tcp",
+                "ports": "3306",
+                "destinations": {"droplet_ids": [49696269]},
+            }
+        ],
+    }
+
+    mock_client.firewalls.delete_rules(firewall_id="aaa-bbb-111-ccc-222", body=rules)
