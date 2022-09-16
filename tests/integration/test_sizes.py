cfb52ac143cfdd96218db43b8e7a73dfac371329
Andrew Starr-Bochicchio <andrewsomething@users.noreply.github.com>
2022-09-16 11:27:24 -0400

    Add some basic testing of the aio client (#88)

--- tests/integration/test_sizes.py
+++ tests/integration/test_sizes.py
@@ -1,12 +1,22 @@
 """ test_sizes.py
     Integration Test for Sizes
 """
+import pytest
 
 from digitalocean import Client
+from digitalocean.aio import Client as aioClient
 
 
 def test_sizes_list(integration_client: Client):
     """Testing the List of the Sizes endpoint"""
     list_resp = integration_client.sizes.list()
+
+    assert len(list_resp["sizes"]) >= 20
+
+
+@pytest.mark.asyncio
+async def test_sizes_list_async(async_integration_client: aioClient):
+    """Testing the List of the Sizes endpoint"""
+    list_resp = await async_integration_client.sizes.list()
 
     assert len(list_resp["sizes"]) >= 20
--- tests/mocked/test_account.py
+++ tests/mocked/test_account.py
@@ -1,28 +1,46 @@
 """Mock tests for the account API resource."""
+import pytest
 import responses
+from aioresponses import aioresponses
 
 from digitalocean import Client
+from digitalocean.aio import Client as aioClient
+
+EXPECTED_ACCOUNT = {
+    "account": {
+        "droplet_limit": 25,
+        "floating_ip_limit": 5,
+        "email": "sammy@digitalocean.com",
+        "uuid": "b6fr89dbf6d9156cace5f3c78dc9851d957381ef",
+        "email_verified": True,
+        "status": "active",
+        "status_message": " ",
+        "team": {
+            "uuid": "5df3e3004a17e242b7c20ca6c9fc25b701a47ece",
+            "name": "My Team",
+        },
+    }
+}
 
 
 @responses.activate
-def test_get(mock_client: Client, mock_client_url):
+def test_account_get(mock_client: Client, mock_client_url):
     """Mocks the account get operation."""
-    expected = {
-        "account": {
-            "droplet_limit": 25,
-            "floating_ip_limit": 5,
-            "email": "sammy@digitalocean.com",
-            "uuid": "b6fr89dbf6d9156cace5f3c78dc9851d957381ef",
-            "email_verified": True,
-            "status": "active",
-            "status_message": " ",
-            "team": {
-                "uuid": "5df3e3004a17e242b7c20ca6c9fc25b701a47ece",
-                "name": "My Team",
-            },
-        }
-    }
-    responses.add(responses.GET, f"{mock_client_url}/v2/account", json=expected)
+
+    responses.add(responses.GET, f"{mock_client_url}/v2/account", json=EXPECTED_ACCOUNT)
     acct = mock_client.account.get()
 
-    assert acct == expected
+    assert acct == EXPECTED_ACCOUNT
+
+
+@pytest.mark.asyncio
+async def test_account_get_async(mock_aio_client: aioClient, mock_client_url):
+    """Mocks the account get aio operation."""
+
+    with aioresponses() as mock_resp:
+        mock_resp.get(
+            f"{mock_client_url}/v2/account", status=200, payload=EXPECTED_ACCOUNT
+        )
+        acct = await mock_aio_client.account.get()
+
+        assert acct == EXPECTED_ACCOUNT
