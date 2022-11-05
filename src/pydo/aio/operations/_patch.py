9acb841f1cc59d8c866ee971f16f873544d4a4c2
Cesar Garza <scotch.neat@live.com>
2022-11-05 13:23:06 -0500

    Update and format generated client

--- src/pydo/aio/operations/_patch.py
+++ src/pydo/aio/operations/_patch.py
@@ -12,7 +12,9 @@ if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
     from typing import List
 
-__all__ = []  # type: List[str]  # Add all objects you want publicly available to users at this package level
+__all__ = (
+    []
+)  # type: List[str]  # Add all objects you want publicly available to users at this package level
 
 
 def patch_sdk():
