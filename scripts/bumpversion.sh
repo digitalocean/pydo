2359e24e63f5c24ede516f95e37577d0c50b1e19
Cesar Garza <scotch.neat@live.com>
2022-11-08 09:56:46 -0600

    Address feedback and include git status checks

--- scripts/bumpversion.sh
+++ scripts/bumpversion.sh
@@ -19,15 +19,13 @@ case "$BUMP" in
     ;;
 esac
 
-# if [[ $(git status --porcelain) != "" ]]; then
-#   echo "Error: repo is dirty. Run git status, clean repo and try again."
-#   exit 1
-# elif [[ $(git status --porcelain -b | grep -e "ahead" -e "behind") != "" ]]; then
-#   echo "Error: repo has unpushed commits. Push commits to remote and try again."
-#   exit 1
-# fi  
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [[ $(git status --porcelain) != "" ]]; then
+  echo "Error: repo is dirty. Run git status, clean repo and try again."
+  exit 1
+elif [[ $(git status --porcelain -b | grep -e "ahead" -e "behind") != "" ]]; then
+  echo "Error: repo has unpushed commits. Bumping the version should not include other changes."
+  exit 1
+fi  
 
 poetry_version=$(poetry version)
 version="${poetry_version:5}"
