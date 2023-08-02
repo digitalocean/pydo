7e6e9adc8dce1e4c9e7b2315208293d79bcedc06
danaelhe <42972711+danaelhe@users.noreply.github.com>
2023-08-02 16:58:50 -0400

    Prep 0.1.6 Release (#186)

--- src/pydo/_version.py
+++ src/pydo/_version.py
@@ -4,4 +4,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "0.1.5"
+VERSION = "0.1.6"
