f4809eb871a4f64d0e2c11b1121f2c1dc1bed27d
Andrew Starr-Bochicchio <andrewsomething@users.noreply.github.com>
2023-08-24 11:10:19 -0400

    Prep release v0.1.7 (#190)

--- src/pydo/_version.py
+++ src/pydo/_version.py
@@ -4,4 +4,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "0.1.6"
+VERSION = "0.1.7"
