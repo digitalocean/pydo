a7e608ba7345b740a6b98256127bc1265a1e50aa
anup-deka <adeka@digitalocean.com>
2026-02-23 19:25:00 +0530

    Releasing v0.27.0 (#608)

--- src/pydo/_version.py
+++ src/pydo/_version.py
@@ -4,4 +4,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "0.26.0"
+VERSION = "0.27.0"
