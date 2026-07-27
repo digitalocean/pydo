22f1d4720e39b35d7edc6ee93a6ee0d098457f1a
Tyler Gillam <tgillam@digitalocean.com>
2026-07-27 10:53:04 -0500

    update

---
+++ examples/gateway/create_session.py
@@ -0,0 +1,19 @@
+"""Create an Action Gateway session and print its MCP URL.
+
+Required env:
+  DIGITALOCEAN_TOKEN
+
+Optional env:
+  ACTOR_ID
+"""
+
+import os
+
+from pydo.action_gateway import ActionGatewayClient
+
+client = ActionGatewayClient(token=os.environ["DIGITALOCEAN_TOKEN"])
+session = client.session.create(
+    actor_id=os.environ.get("ACTOR_ID", "example-user"),
+)
+
+print(session.url)
